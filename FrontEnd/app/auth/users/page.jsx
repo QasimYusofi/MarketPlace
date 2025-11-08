facb759518814c10d051d2363ce95ccb8364bab3
FARTASH-421 <abdul.qhadir.fartash@gmail.com>
2025-11-09 02:12:47 +0330

    Create new file for Logi page users

---
+++ FrontEnd/app/auth/users/page.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const LoginPage = () => {
+  return (
+    <div className="h-screen w-screen">
+      <div className="h-full w-full flex items-center justify-center">
+        <div className="flex  items-center justify-center bg-amber-600 px-10 py-2 rounded-2xl shadow-lg">
+          <h3>LoginPage</h3>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default LoginPage;
--- FrontEnd/app/page.js
+++ FrontEnd/app/page.js
@@ -1,8 +1,14 @@
+import Link from "next/link";
 import React from "react";
 
 const HomePage = () => {
   return (
     <div className="h-screen w-screen flex items-center justify-center">
+      <div className="absolute top-0 left-1/2 h-16 w-32 z-0 font-bold rounded-3xl flex justify-center items-center m-2 bg-amber-100">
+        <Link href="/auth/users/" className="absolute text-blue-600 underline">
+          Login Page
+        </Link>
+      </div>
       <div className="text-center bg-blue-300 p-8 rounded-lg shadow-lg">
         <h1 className="text-4xl font-bold mb-4">Welcome to the Home Page</h1>
         <p className="text-lg text-white font-bold">
